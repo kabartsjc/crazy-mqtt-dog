a5deeccb3c4191aa8a89e1e281dc10d4ac2c6516
Alexandre B Barreto <kabart@gmail.com>
2022-09-18 18:01:52 -0300

    Add install script

--- install-scripts/script_install.sh
+++ install-scripts/script_install.sh
@@ -2,19 +2,29 @@
 #!/bin/bash -x
 echo "Install package dependencies - crazy-mqtt-dog"
 echo "Initialization"
-for (( counter=10; counter>0; counter-- ))
+for (( counter=100; counter>0; counter-- ))
 do
-echo -n ".."
+echo -n "....."
 done
 printf "\n"
-echo -e "\nRemoving \t backslash \t characters\n"
 
+
+echo "Ubuntu update ...."
 sudo apt-get update
+echo "Ubuntu upgrade ...."
 sudo apt-get upgrade
+
+echo "Java installation ...."
 sudo apt install -y openjdk-18-jre-headless
+
+echo "Python 3 installation ...."
 sudo apt-get install -y python3
 sudo apt-get install -y python-pip
+
+echo "Git installation ...."
 sudo apt-get install -y git
+
+echo "Mosquitto 2 installation ...."
 sudo apt-get install -y mosquitto
 sudo apt-get install -y mosquitto-clients
 sudo apt-get install -y vim
